c5e3f94d2d40f9b6a525a329fd33c0e2a0ff2a2a
almiraj-hz <62835935+almiraj-hz@users.noreply.github.com>
2020-06-28 12:03:03 +0800

    Update settings.py

--- dailyfresh/settings.py
+++ dailyfresh/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'hjh',
         'USER': 'root',
-        'PASSWORD': 'hanjianhao35*',
+        'PASSWORD': '************',
         'HOST': '127.0.0.1',
         'PORT': '3306',
         'OPTIONS': {'init_command': "SET sql_mode='STRICT_TRANS_TABLES'", 'charset': 'utf8', },
